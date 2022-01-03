3175009411b088afc3a81609b9dcd5b888c6cb7a
Nur Syahfei <nur.syahfei@gmail.com>
2022-01-03 11:27:29 +0700

    refactoring: make button_login.dart & form_login.dart in own file

---
+++ lib/widget/button_login.dart
@@ -0,0 +1,70 @@
+
+import 'package:flutter/material.dart';
+import 'package:java_code_app/thame/colors.dart';
+import 'package:java_code_app/thame/spacing.dart';
+import 'package:java_code_app/thame/text_style.dart';
+
+class ButtonLogin extends StatelessWidget {
+  final String? icon;
+  final Color bgColors;
+  final String title;
+
+  const ButtonLogin({
+    Key? key,
+    this.icon,
+    this.boldTitle,
+    required this.bgColors,
+    required this.title,
+  }) : super(key: key);
+
+  final String? boldTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        primary: bgColors,
+        onPrimary: bgColors == ColorSty.white ? ColorSty.primary : ColorSty.white,
+        padding: const EdgeInsets.symmetric(vertical: SpaceDims.sp16),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(144.0),
+        ),
+      ),
+      child: Container(
+        alignment: Alignment.center,
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(horizontal: SpaceDims.sp8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (icon != null)
+              Padding(
+                padding: const EdgeInsets.only(right: SpaceDims.sp12),
+                child: Image.asset(icon!),
+              ),
+            Text(
+              title,
+              style: TypoSty.button.copyWith(
+                fontWeight: boldTitle == null ? FontWeight.bold : null,
+                color: ColorSty.white != bgColors
+                    ? ColorSty.white
+                    : ColorSty.black,
+              ),
+            ),
+            if (boldTitle != null)
+              Text(
+                " " + boldTitle!,
+                style: TypoSty.button.copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: ColorSty.white != bgColors
+                      ? ColorSty.white
+                      : ColorSty.black,
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
