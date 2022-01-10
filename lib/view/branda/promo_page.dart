56cbfb2a597b454b7454f07b7666f8e3ed1f55fc
Nur Syahfei <nur.syahfei@gmail.com>
2022-01-10 18:57:25 +0700

    styling promo: implement screenutil & adaptive

--- lib/view/branda/promo_page.dart
+++ lib/view/branda/promo_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:java_code_app/theme/colors.dart';
 import 'package:java_code_app/theme/icons_cs_icons.dart';
 import 'package:java_code_app/theme/spacing.dart';
@@ -19,121 +20,153 @@ class PromoPage extends StatelessWidget {
         icon: Icon(IconsCs.coupon, color: ColorSty.primary),
         title: 'Promo',
       ),
-      body: Column(
-        children: [
-          const SizedBox(height: SpaceDims.sp24),
-          const CardCoupon(),
-          const SizedBox(height: SpaceDims.sp24),
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                  color: ColorSty.white,
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(30.0),
-                    topRight: Radius.circular(20.0),
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      offset: const Offset(0, -1),
-                      color: ColorSty.grey.withOpacity(0.01),
-                      spreadRadius: 1,
-                    )
-                  ]),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        top: SpaceDims.sp24,
-                        bottom: SpaceDims.sp12,
-                        left: SpaceDims.sp24,
-                        right: SpaceDims.sp24,
+      body: ScreenUtilInit(builder: () {
+        return Column(
+          children: [
+            SizedBox(height: SpaceDims.sp24.h),
+            const CardCoupon(),
+            SizedBox(height: SpaceDims.sp24.h),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: ColorSty.white,
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(30.0),
+                      topRight: Radius.circular(20.0),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: const Offset(0, -1),
+                        color: ColorSty.grey.withOpacity(0.01),
+                        spreadRadius: 1,
+                      )
+                    ]),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(
+                          top: SpaceDims.sp24.h,
+                          bottom: SpaceDims.sp12.h,
+                          left: SpaceDims.sp24.w,
+                          right: SpaceDims.sp24.w,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Nama Promo", style: TypoSty.title),
+                            Text(
+                              "Diskon 10%",
+                              style: TypoSty.title.copyWith(
+                                color: ColorSty.primary,
+                              ),
+                            )
+                          ],
+                        ),
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      Container(
+                        height: 1,
+                        width: double.infinity,
+                        margin: EdgeInsets.symmetric(
+                          horizontal: SpaceDims.sp24.h,
+                        ),
+                        color: ColorSty.grey,
+                      ),
+                      const SizedBox(height: SpaceDims.sp12),
+                      Row(
                         children: [
-                          Text("Nama Promo", style: TypoSty.title),
-                          Text("Diskon 10%",
-                              style: TypoSty.title
-                                  .copyWith(color: ColorSty.primary))
+                          SizedBox(width: SpaceDims.sp24.w),
+                          const Icon(
+                            Icons.list,
+                            color: ColorSty.primary,
+                            size: 32.0,
+                          ),
+                          SizedBox(width: SpaceDims.sp12.w),
+                          Column(
+                            children: [
+                              Text(
+                                "Syarat dan Ketentuan",
+                                style: TypoSty.subtitle,
+                              ),
+                            ],
+                          )
                         ],
                       ),
-                    ),
-                    Container(
-                      height: 1,
-                      width: double.infinity,
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: SpaceDims.sp24),
-                      color: ColorSty.grey,
-                    ),
-                    const SizedBox(height: SpaceDims.sp12),
-                    Row(
-                      children: [
-                        const SizedBox(width: SpaceDims.sp24),
-                        const Icon(Icons.list,
-                            color: ColorSty.primary, size: 32.0),
-                        const SizedBox(width: SpaceDims.sp12),
-                        Column(
+                      Padding(
+                        padding: EdgeInsets.only(
+                          right: SpaceDims.sp16.w,
+                          left: 72.0.w,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(
-                              "Syarat dan Ketentuan",
-                              style: TypoSty.subtitle,
+                            const Text(
+                              """Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea vommodo consequat.
+                                """,
+                              textAlign: TextAlign.left,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: SpaceDims.sp12.w),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: const [
+                                      Text("1. "),
+                                      Expanded(
+                                        child: Text(
+                                          "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et",
+                                          textAlign: TextAlign.left,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(height: SpaceDims.sp12.h),
+                                  Row(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: const [
+                                      Text("2. "),
+                                      Expanded(
+                                        child: Text(
+                                          "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et",
+                                          textAlign: TextAlign.left,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(height: SpaceDims.sp12.h),
+                                  Row(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: const [
+                                      Text("3. "),
+                                      Expanded(
+                                        child: Text(
+                                          "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et",
+                                          textAlign: TextAlign.left,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: SpaceDims.sp12.h),
+                            const Text(
+                              """Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea vommodo consequat.
+                                """,
+                              textAlign: TextAlign.left,
                             ),
                           ],
-                        )
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        right: SpaceDims.sp16,
-                        left: 72.0,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            """Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea vommodo consequat.
-                            """,
-                            textAlign: TextAlign.left,
-                          ),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(left: SpaceDims.sp12),
-                            child: Column(
-                              children: const [
-                                Text(
-                                  "1. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et",
-                                  textAlign: TextAlign.left,
-                                ),
-                                SizedBox(height: SpaceDims.sp12),
-                                Text(
-                                  "2. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et",
-                                  textAlign: TextAlign.left,
-                                ),
-                                SizedBox(height: SpaceDims.sp12),
-                                Text(
-                                  "3. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et",
-                                  textAlign: TextAlign.left,
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(height: SpaceDims.sp12),
-                          const Text(
-                            """Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea vommodo consequat.
-                            """,
-                            textAlign: TextAlign.left,
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
-      ),
+          ],
+        );
+      }),
     );
   }
 }
