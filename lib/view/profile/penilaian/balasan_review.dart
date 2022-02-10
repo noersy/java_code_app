4739c7dba0c3cbd8376d5f90076fc3fb3f762f61
mlatifr <mlatifraldi@gmail.com>
2022-02-10 16:05:14 +0700

    BtmTextField Decoration

--- lib/view/profile/penilaian/balasan_review.dart
+++ lib/view/profile/penilaian/balasan_review.dart
@@ -1,11 +1,6 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:java_code_app/providers/lang_providers.dart';
-import 'package:java_code_app/theme/spacing.dart';
 import 'package:java_code_app/widget/appbar/appbar.dart';
-import 'package:provider/provider.dart';
 
 class BalasanReview extends StatefulWidget {
   const BalasanReview({Key? key}) : super(key: key);
@@ -20,8 +15,8 @@ class _BalasanReviewState extends State<BalasanReview> {
     return AnimatedBuilder(
         animation: LangProviders(),
         builder: (context, snapshot) {
-          final lang = Provider.of<LangProviders>(context).lang;
           return Scaffold(
+            backgroundColor: Color.fromARGB(255, 229, 229, 229),
             appBar: const CostumeAppBar(
               title: '',
               profileTitle: 'BalasanReview',
@@ -33,10 +28,10 @@ class _BalasanReviewState extends State<BalasanReview> {
               child: Container(
                 // height: 300.0,
                 // height: MediaQuery.of(context).size.height / 1.3,
-                color: Colors.transparent,
+                // color: Color.fromARGB(255, 98, 97, 97),
                 child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.green,
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
                         borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(40.0),
                           topRight: Radius.circular(40.0),
@@ -44,22 +39,49 @@ class _BalasanReviewState extends State<BalasanReview> {
                           bottomRight: Radius.circular(40.0),
                         )),
                     child: Center(
-                      child: Text("Hi modal sheet"),
-                    )),
+                        child: Image.asset(
+                            "assert/image/bg_daftarpenilaian.png"))),
               ),
             ),
-            bottomNavigationBar: ConstrainedBox(
-                constraints: BoxConstraints.tight(const Size(200, 50)),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(hintText: 'Name'),
+            bottomNavigationBar: Container(
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                  )),
+              child: ConstrainedBox(
+                  constraints: BoxConstraints.tight(const Size(200, 50)),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              left: 10.0, right: 5, top: 5, bottom: 5),
+                          child: Container(
+                            decoration: const BoxDecoration(
+                                color: Colors.blue,
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(40.0),
+                                  topRight: Radius.circular(40.0),
+                                  bottomLeft: Radius.circular(40.0),
+                                  bottomRight: Radius.circular(40.0),
+                                )),
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 8.0),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    border: InputBorder.none, hintText: 'Name'),
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
-                    ElevatedButton(onPressed: () {}, child: Icon(Icons.send)),
-                  ],
-                )),
+                      ElevatedButton(
+                          onPressed: () {}, child: const Icon(Icons.send)),
+                    ],
+                  )),
+            ),
           );
         });
   }
