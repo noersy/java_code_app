2363d4d5d7e06afdb35480fe8e911c169af4af58
mlatifr <mlatifraldi@gmail.com>
2022-02-15 11:53:04 +0700

    simpan hasil API ke list, next tampilin ke apk

--- lib/view/profile/penilaian/daftar_penilaian.dart
+++ lib/view/profile/penilaian/daftar_penilaian.dart
@@ -1,14 +1,10 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:java_code_app/models/listreview.dart';
 import 'package:java_code_app/providers/lang_providers.dart';
-import 'package:java_code_app/providers/rating_providers.dart';
 import 'package:java_code_app/route/route.dart';
 import 'package:java_code_app/widget/appbar/appbar.dart';
-import 'package:provider/provider.dart';
-
 import 'fetch_rating.dart';
 
 class DaftarPenilaian extends StatefulWidget {
@@ -20,14 +16,20 @@ class DaftarPenilaian extends StatefulWidget {
 
 class _DaftarPenilaianState extends State<DaftarPenilaian>
     with TickerProviderStateMixin {
-  Future loadReview() async {
+  List _listReview = [];
+  loadReview() {
     print('loadReview: ');
     Future data = getAllReview();
     data.then((value) {
       Map json = jsonDecode(value);
       for (var i in json['data']) {
         print('loadReview i: $i');
+        Review rv = Review.fromJson(i);
+        _listReview.add(i);
       }
+      setState(() {
+        widgetListReview();
+      });
     });
     // Future data = Review;
     //     .then((value) {
@@ -82,125 +84,71 @@ class _DaftarPenilaianState extends State<DaftarPenilaian>
                   child: SingleChildScrollView(
                     child: Column(
                       children: [
-                        ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: 5,
-                            itemBuilder: (context, index) {
-                              return GestureDetector(
-                                onTap: () => Navigate.toBalasanReview(context),
-                                child: Column(
-                                  children: [
-                                    Container(
-                                      decoration: BoxDecoration(
-                                        color: const Color(0xFFF6F6F6),
-                                        borderRadius: BorderRadius.circular(10),
-                                      ),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Padding(
-                                            padding: const EdgeInsets.only(
-                                                left: 10, top: 10),
-                                            child: Row(
-                                              children: [
-                                                SvgPicture.asset(
-                                                  "assert/image/icons/kalender.svg",
-                                                ),
-                                                SizedBox(
-                                                  width: 10,
-                                                ),
-                                                Text(
-                                                  'Penyajian Makanan',
-                                                  style: TextStyle(
-                                                      color: Color.fromARGB(
-                                                          255, 1, 154, 173)),
-                                                ),
-                                                Image.asset(
-                                                    'assert/image/icons/star_yellow.png'),
-                                                Image.asset(
-                                                    'assert/image/icons/star_yellow.png'),
-                                                Image.asset(
-                                                    'assert/image/icons/star_yellow.png'),
-                                                Image.asset(
-                                                    'assert/image/icons/star_yellow.png'),
-                                                SvgPicture.asset(
-                                                  "assert/image/icons/star_gray.svg",
-                                                ),
-                                              ],
-                                            ),
-                                          ),
-                                          const Padding(
-                                            padding: EdgeInsets.only(
-                                                left: 10, bottom: 10),
-                                            child: Text(
-                                              'keterangan  ',
-                                              textAlign: TextAlign.start,
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                    ),
-                                    const SizedBox(
-                                      height: 10,
-                                    ),
-                                  ],
-                                ),
-                              );
-                            }),
-                        // Container(
-                        //   decoration: BoxDecoration(
-                        //     color: const Color(0xFFF6F6F6),
-                        //     borderRadius: BorderRadius.circular(10),
-                        //   ),
-                        //   child: Column(
-                        //     crossAxisAlignment: CrossAxisAlignment.start,
-                        //     children: [
-                        //       Padding(
-                        //         padding:
-                        //             const EdgeInsets.only(left: 10, top: 10),
-                        //         child: Row(
-                        //           children: [
-                        //             SvgPicture.asset(
-                        //               "assert/image/icons/kalender.svg",
-                        //             ),
-                        //             Text(
-                        //               'Penyajian Makanan',
-                        //               style: TextStyle(
-                        //                   color:
-                        //                       Color.fromARGB(255, 1, 154, 173)),
-                        //             ),
-                        //             Image.asset(
-                        //                 'assert/image/icons/star_yellow.png'),
-                        //             Image.asset(
-                        //                 'assert/image/icons/star_yellow.png'),
-                        //             Image.asset(
-                        //                 'assert/image/icons/star_yellow.png'),
-                        //             Image.asset(
-                        //                 'assert/image/icons/star_yellow.png'),
-                        //             SvgPicture.asset(
-                        //               "assert/image/icons/star_gray.svg",
-                        //             ),
-                        //           ],
-                        //         ),
-                        //       ),
-                        //       Padding(
-                        //         padding:
-                        //             const EdgeInsets.only(left: 10, bottom: 10),
-                        //         child: Text(
-                        //           'keterangan  ',
-                        //           textAlign: TextAlign.start,
-                        //         ),
-                        //       )
-                        //     ],
-                        //   ),
-                        // ),
-                        // SizedBox(
-                        //   height: 10,
-                        // ),
+                        widgetListReview(),
                       ],
                     ),
                   ),
+                ),
+              ],
+            ),
+          );
+        });
+  }
+
+  ListView widgetListReview() {
+    return ListView.builder(
+        shrinkWrap: true,
+        itemCount: _listReview.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () => Navigate.toBalasanReview(context),
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFF6F6F6),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10, top: 10),
+                        child: Row(
+                          children: [
+                            SvgPicture.asset(
+                              "assert/image/icons/kalender.svg",
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              '${_listReview[index].type}',
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 1, 154, 173)),
+                            ),
+                            Image.asset('assert/image/icons/star_yellow.png'),
+                            Image.asset('assert/image/icons/star_yellow.png'),
+                            Image.asset('assert/image/icons/star_yellow.png'),
+                            Image.asset('assert/image/icons/star_yellow.png'),
+                            SvgPicture.asset(
+                              "assert/image/icons/star_gray.svg",
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(left: 10, bottom: 10),
+                        child: Text(
+                          'keterangan  ',
+                          textAlign: TextAlign.start,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
                 ),
               ],
             ),
