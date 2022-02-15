de123cd1bc560b8a3b1a03c6f1631cd38da5b8c9
mlatifr <mlatifraldi@gmail.com>
2022-02-15 15:31:45 +0700

    selectedType

--- lib/view/profile/penilaian/penilaian.dart
+++ lib/view/profile/penilaian/penilaian.dart
@@ -123,93 +123,7 @@ class _PenilaianState extends State<Penilaian> {
                             const SizedBox(
                               height: 10,
                             ),
-                            Container(
-                                width: MediaQuery.of(context).size.height,
-                                decoration: BoxDecoration(
-                                  color: const Color(0xFFF6F6F6),
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                child: Column(
-                                  children: [
-                                    const Text('Apa yang bisa ditingkatkan?'),
-                                    Wrap(
-                                      // mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        TextButton(
-                                            onPressed: () {},
-                                            child: const Text('Harga')),
-                                        TextButton(
-                                            onPressed: () {},
-                                            child: const Text('Rasa')),
-                                        TextButton(
-                                            onPressed: () {},
-                                            child: const Text(
-                                                'Penyajian Makanan')),
-                                        TextButton(
-                                            onPressed: () {},
-                                            child: const Text('Pelayanan')),
-                                        TextButton(
-                                            onPressed: () {},
-                                            child: const Text('Fasilitas')),
-                                      ],
-                                    ),
-                                    const Padding(
-                                      padding: EdgeInsets.all(8.0),
-                                      child: Divider(
-                                        thickness: 2,
-                                      ),
-                                    ),
-                                    const Text('Tulis Review'),
-                                    Padding(
-                                      padding: const EdgeInsets.all(10.0),
-                                      child: TextFormField(
-                                        style: const TextStyle(fontSize: 12),
-                                        maxLines: 5,
-                                        initialValue:
-                                            'Mohon Menjaga Kebersihan, Kemarin Meja Masih Kotor',
-                                        decoration: const InputDecoration(
-                                            border: OutlineInputBorder(),
-                                            hintText: '',
-                                            fillColor: Colors.white,
-                                            filled: true),
-                                      ),
-                                    ),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        ElevatedButton(
-                                            onPressed: () {
-                                              postPenilaian(score, 'Fasilitas',
-                                                  'review fasilitas');
-                                            },
-                                            child:
-                                                const Text('Kirim Penilaian')),
-                                        RawMaterialButton(
-                                          onPressed: () {},
-                                          elevation: 2.0,
-                                          fillColor: Colors.white,
-                                          child: const Icon(
-                                            Icons.pause,
-                                            size: 12.0,
-                                          ),
-                                          padding: const EdgeInsets.all(15.0),
-                                          shape: const CircleBorder(),
-                                        )
-                                      ],
-                                    )
-                                    // TextFormField(
-                                    //   style: TextStyle(fontSize: 12),
-                                    //   maxLines: 5,
-                                    //   initialValue:
-                                    //       'Mohon Menjaga Kebersihan, Kemarin Meja Masih Kotor',
-                                    //   decoration: InputDecoration(
-                                    //       border: OutlineInputBorder(),
-                                    //       hintText: '',
-                                    //       fillColor: Colors.white),
-                                    // )
-                                  ],
-                                )),
+                            widgetType(context),
                           ],
                         )
                       ],
@@ -220,6 +134,117 @@ class _PenilaianState extends State<Penilaian> {
             ),
           );
         });
+  }
+
+  List listType = [
+    'Harga'
+        'Rasa'
+        'Penyajian Makanan'
+        'Pelayanan'
+        'Fasilitas'
+  ];
+  Container widgetType(BuildContext context) {
+    return Container(
+        width: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          color: const Color(0xFFF6F6F6),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          children: [
+            const Text('Apa yang bisa ditingkatkan?'),
+            Wrap(
+              children: [
+                for (var i in listType) widgetButtonType(i),
+              ],
+            ),
+            // Wrap(
+            //   // mainAxisSize: MainAxisSize.min,
+            //   children: [
+            //     TextButton(onPressed: () {}, child: const Text('Harga')),
+            //     TextButton(onPressed: () {}, child: const Text('Rasa')),
+            //     TextButton(
+            //         onPressed: () {}, child: const Text('Penyajian Makanan')),
+            //     TextButton(onPressed: () {}, child: const Text('Pelayanan')),
+            //     TextButton(onPressed: () {}, child: const Text('Fasilitas')),
+            //   ],
+            // ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Divider(
+                thickness: 2,
+              ),
+            ),
+            const Text('Tulis Review'),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                style: const TextStyle(fontSize: 12),
+                maxLines: 5,
+                initialValue:
+                    'Mohon Menjaga Kebersihan, Kemarin Meja Masih Kotor',
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: '',
+                    fillColor: Colors.white,
+                    filled: true),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      postPenilaian(score, 'Fasilitas', 'review fasilitas');
+                    },
+                    child: const Text('Kirim Penilaian')),
+                RawMaterialButton(
+                  onPressed: () {},
+                  elevation: 2.0,
+                  fillColor: Colors.white,
+                  child: const Icon(
+                    Icons.pause,
+                    size: 12.0,
+                  ),
+                  padding: const EdgeInsets.all(15.0),
+                  shape: const CircleBorder(),
+                )
+              ],
+            )
+            // TextFormField(
+            //   style: TextStyle(fontSize: 12),
+            //   maxLines: 5,
+            //   initialValue:
+            //       'Mohon Menjaga Kebersihan, Kemarin Meja Masih Kotor',
+            //   decoration: InputDecoration(
+            //       border: OutlineInputBorder(),
+            //       hintText: '',
+            //       fillColor: Colors.white),
+            // )
+          ],
+        ));
+  }
+
+  var selectedType = '';
+  Widget widgetButtonType(i) {
+    if (i == selectedType) {
+      return TextButton(
+        onPressed: () {},
+        child: Text('${i}'),
+      );
+    } else {
+      return TextButton(
+        style: TextButton.styleFrom(
+          primary: Color.fromARGB(255, 170, 170, 170),
+        ),
+        onPressed: () {
+          setState(() {
+            selectedType = i;
+          });
+        },
+        child: Text('${i}'),
+      );
+    }
   }
 
   String RatingText = 'rating now';
