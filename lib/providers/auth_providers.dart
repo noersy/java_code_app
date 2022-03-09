9ac11779e4ae1abe62e55e97708ae114f47d7c58
mlatifr <mlatifraldi@gmail.com>
2022-03-09 11:21:47 +0700

    remove unused print

--- lib/providers/auth_providers.dart
+++ lib/providers/auth_providers.dart
@@ -96,15 +96,8 @@ class AuthProviders extends ChangeNotifier {
         if (json.decode(response.body)["data"]["user"]["foto"] == null) {
           editResponse["data"]["user"]["foto"] =
               '''https://javacode.landa.id/img/1/review/review_1_620e0269b96d2.png''';
-          print(
-              'cek foto ${editResponse["data"]["user"]["foto"]} | editResponse type: ${editResponse.runtimeType}\n\n\n');
-          print('cek editResponse ${(editResponse)} '
-              '\nbody ${response.body}'
-              '\neditResponse ${json.encode(editResponse)}');
           editResponse = json.encode(editResponse);
         }
-
-        // _loginUser = loginUserFromJson(response.body);
         _loginUser = loginUserFromJson(editResponse.toString());
         if (_loginUser == null) _log.info("Login failed");
         if (_loginUser != null) _log.fine("Login successes");
@@ -113,8 +106,6 @@ class AuthProviders extends ChangeNotifier {
             .setIntValue(KeyPrefens.loginID, _loginUser!.data.user.idUser);
         getUser(id: _loginUser!.data.user.idUser);
         notifyListeners();
-
-        print('GoogleSignInAccount: loginGoogle ${_loginUser!.data.user.foto}');
         if (_loginUser != null) {
           return true;
         } else {
@@ -162,13 +153,11 @@ class AuthProviders extends ChangeNotifier {
 
     final _id = user.data.idUser;
     final Uri _api = Uri.http(host, "/api/user/update/$_id");
-    // print('ker: $key | value: $value');
     try {
       final body = jsonEncode({"$key": "$value"});
 
       _log.fine("Tray update user profile.");
       final response = await http.post(_api, headers: _headers, body: body);
-      // print('response edit nama ${response.body}');
       if (response.statusCode == 200 &&
           json.decode(response.body)["status_code"] == 200) {
         _log.fine("Success update $key user.");
